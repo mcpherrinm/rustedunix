158003b80f1b9070e6612ca293fbba61cefe09f3
Matthew McPherrin <matthew@mcpherrin.ca>
2013-01-24 01:43:00 -0500

    Add command line parsing

--- seq.rs
+++ seq.rs
@@ -1,6 +1,4 @@
 fn seq(start: float, incr: float, end: float, sep: &str) {
-  io::println(fmt!("start %f, incr %f, end %f", start, incr, end));
-
   let mut i: int = 0;
   let count = (end - start)/incr as int;
   if(count < 0) {
@@ -13,9 +11,37 @@ fn seq(start: float, incr: float, end: float, sep: &str) {
   }
 }
 
+fn usage() {
+  io::println("seq LAST");
+  io::println("seq FIRST LAST");
+  io::println("seq FIRST INCR LAST");
+}
+
+macro_rules! setif(
+  ($setme:ident, $len:expr) => (
+    if(len > $len) {
+      $setme = float::from_str(args[len-$len]).get()
+    }
+  )
+)
 
 fn main() {
+  let args = os::args();
+  let sep = ~"\n";
 
+  let mut start = 1f;
+  let mut incr = 1f;
+  let mut end = 1f;
 
-  seq(1f, 0.5f, 5f, ~"\n");
+  let len = args.len();
+  if(len == 1) {
+    usage();
+    return
+  }
+
+  setif!(start, 3);
+  setif!(end, 1);
+  setif!(incr, 2);
+
+  seq(start, incr, end, sep);
 }
